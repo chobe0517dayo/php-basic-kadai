56d5a52bebe533f8cf44849091887bcb8ba0d2f6
chobe0517dayo <148618112+chobe0517dayo@users.noreply.github.com>
2023-12-27 14:17:50 +0900

    kadai_sort アップロード２

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -9,8 +9,13 @@
 <body>
     <p>
         <?php
+        function sort_asort() {
+        
         // ソートする配列を宣言
         $nums = [15, 4, 18, 23, 10 ];
+
+
+        
 
         if (true) {
             echo '昇順にソートします。<br>';
@@ -18,14 +23,18 @@
             foreach ($nums as $num){
             echo "$num <br>";
             }
-        } elseif (false) {
+        } else {
             echo '降順にソートします。<br>';
             arsort($nums);
             foreach ($nums as $num); {
             echo "$num <br>";
             }
         }
-        
+        }
+
+        sort_asort();
+
+        sort_asort();
 
         ?>
     </p>

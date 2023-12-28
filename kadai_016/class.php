f242202cd716091f13c4b07fd7d7f88fdaa8fd0f
chobe0517dayo <148618112+chobe0517dayo@users.noreply.github.com>
2023-12-28 16:19:12 +0900

    kadai_016アップロード１

---
+++ kadai_016/class.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>kadai_016</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        // クラスを定義する
+        class Food {
+            //プロパティを定義する
+            private $name;
+            private $price;
+
+            // コントラクタを定義する
+            public function __construct(string $name, int $price) {
+                $this->name = $name;
+                $this->price = $price;
+
+            }
+        }
+
+        // インスタンス化する
+        $food = new Food('potato', 250);
+        
+
+
+
+        // クラスを定義する
+        class Animal {
+            //プロパティを定義する
+            private $name;
+            private $height;
+            private $weight;
+
+            // コントラクタを定義する
+            public function __construct(string $name, int $height, string $weight) {
+                $this->name = $name;
+                $this->height = $height;
+                $this->weight = $weight;
+
+            }
+        }
+
+        // インスタンス化する
+        $animal = new Animal('dog', 60, 5000);
+
+
+
+
+        // インスタンス$userの各プロパティの値を出力する
+        print_r($food);
+        echo '<br>';
+        print_r($animal);
+        echo '<br>';
+        print_r($price);
+
+        
+
+
+
+        ?>
+    </p>
+    
+</body>
+
+</html>
